ba42bcb45e310ca95707cdf48c09b056621c6809
Ngahine Wakefield <28144736+ngas-wakefield@users.noreply.github.com>
2022-03-20 10:36:22 +1300

    added colour npm and git ignore

---
+++ index.js
@@ -0,0 +1,7 @@
+var colour = require('colour');
+console.log('hello'.green); // outputs green text
+
+
+// "scripts": {
+//  "start": "node index.js", 
+//runs node index.js from npm start in terminal
